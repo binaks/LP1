51d724d541aa3c39325da9a8edada330e025ca96
Bianca <bianca.rc@hotmail.com>
2017-08-05 18:25:14 -0300

    add list1902

--- problema_1/lists/list1902.cpp
+++ problema_1/lists/list1902.cpp
@@ -28,3 +28,41 @@ int main()
 	}
 	print_result(count, sum);
 }
+
+/** Prompt the user, then read a number, and ignore the rest of the line.
+ * @param prompt the prompt string
+ * @return the input number or -1 for end-of-file
+ */
+int prompted_read(std::string prompt)
+{
+	std::cout << prompt;
+	int x(-1);
+	std::cin >> x;
+	ignore_line();
+	return x;
+}
+
+/** Ignore the rest of the input line. */
+void ignore_line()
+{
+	char c;
+	while (std::cin.get(c) and c != '\n')
+		/*empty*/;
+}
+
+/** Print the statistics.
+ * @param count the number of values
+ * @param sum the sum of the values
+ */
+void print_result(int count, int sum)
+{
+	if (count == 0)
+	{
+		std::cout << "no data\n";
+		return;
+	}
+	
+	std::cout << "\ncount = " << count;
+	std::cout << "\nsum = " << sum;
+	std::cout << "\nmean = " << sum/count << '\n';
+} 
