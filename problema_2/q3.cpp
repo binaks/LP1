c092a65ea3438c88b496e1b291f187efd66d92ed
JonathanPO <jonathanga2011@gmail.com>
2017-08-08 19:42:05 -0300

    United 13 with fv

--- problema_2/q3.cpp
+++ problema_2/q3.cpp
@@ -1,12 +1,22 @@
 #include <iostream>
 using namespace std;
+
 int fatorial(int n);
+int fatorial_verboso(std::string phrase, int n);
+
 int main(int argc, char const *argv[])
 {
 	int n;
+	char c;
 	cout<<"Informe o valor de n: ";
 	cin>>n;
-	cout<<"Fatorial("<<n<<") = "<<fatorial(n)<<endl;
+	cout << "Modo verboso(s/n)?";
+	cin>>c;
+
+	if(c == 's')
+		cout<< fatorial_verboso("", n) << endl;
+	else
+		cout<<"Fatorial("<<n<<") = "<<fatorial(n)<<endl;
 	
 	return 0;
 }
@@ -18,4 +28,17 @@ int fatorial(int n){
 	else{
 		return fatorial(n-1)*n;
 	}
+}
+
+int fatorial_verboso(string phrase, int n){
+	if(n == 0){
+		cout << phrase << "1\n";
+		return 1;
+	}
+	else{
+		phrase = phrase + std::to_string(n) + " * ";
+		cout << phrase << "fatorial(" << (n-1) << ")\n";
+		return n * fatorial_verboso(phrase, n-1);
+	}
+	
 }
