99d158e0f97f6d557e11340ed56c77b4ecee7c0c
winterScoming <kimura@live.it>
2017-08-08 19:21:48 -0300

    Update q3.cpp

--- problema_2/q3.cpp
+++ problema_2/q3.cpp
@@ -1,13 +1,21 @@
 #include <iostream>
 using namespace std;
-
+int fatorial(int n);
 int main(int argc, char const *argv[])
 {
 	int n;
 	cout<<"Informe o valor de n: ";
 	cin>>n;
-	cout<<"Fatorial("<<n") = "<<fatorial(n)<<endl;
+	cout<<"Fatorial("<<n<<") = "<<fatorial(n)<<endl;
 	
 	return 0;
 }
 
+int fatorial(int n){
+	if(n==1 || n==0){
+		return 1;
+	}
+	else{
+		return fatorial(n-1)*n;
+	}
+}
