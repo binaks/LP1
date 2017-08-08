8b7b25374a15ccd329e680d8b57d08b212a2749b
winterScoming <kimura@live.it>
2017-08-08 19:46:59 -0300

    Create q9.cpp

---
+++ problema_2/q9.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+bool par(int n){
+  
+
+}
+bool impar(int n){
+  
+}
+int main(int argc, char const *argv[])
+{
+	
+	int n;
+	cout<<"Informe o valor de n: ";
+	cin>>n;
+	//função
+	
+	
+
+	return 0;
+}
