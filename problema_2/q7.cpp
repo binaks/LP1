c95396f136510765b161db7eeac6d399c129c967
JonathanPO <jonathanga2011@gmail.com>
2017-08-10 20:32:51 -0300

    Questão 7

---
+++ problema_2/q7.cpp
@@ -0,0 +1,55 @@
+// | (• ◡•)| (❍ᴥ❍ʋ)
+
+#include <iostream>
+using namespace std;
+
+int fatorial(int n);
+int fatorial_verboso(std::string phrase, int n);
+
+int main(int argc, char const *argv[])
+{
+	int n;
+	char c;
+	cout<<"Informe o valor de n: ";
+	cin>>n;
+	cout << "Modo verboso(s/n)?";
+	cin>>c;
+
+	if(c == 's')
+		cout<< fatorial_verboso("", n) << endl;
+	else
+		cout<<"Fatorial("<<n<<") = "<<fatorial(n)<<endl;
+	
+	return 0;
+}
+
+int fatorial(int n){
+	if(n==1 || n==0){
+		return 1;
+	}
+	else{
+		return fatorial(n-1)*n;
+	}
+}
+
+int fatorialIter(int n, int result){
+	if(n == 0){
+		return result;
+	}
+	else{
+		return fatorial(n-1, result*n);
+	}
+}
+
+int fatorial_verboso(string phrase, int n){
+	if(n == 0){
+		cout << phrase << "1\n";
+		return 1;
+	}
+	else{
+		phrase = phrase + std::to_string(n) + " * ";
+		cout << phrase << "fatorial(" << (n-1) << ")\n";
+		return n * fatorial_verboso(phrase, n-1);
+	}
+	
+}
