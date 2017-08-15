1e988654be7f89f13a1f81a472d670b5fd4c92fe
JonathanPO <jonathanga2011@gmail.com>
2017-08-15 20:01:03 -0300

    Add questao13.cpp

---
+++ questao13.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+template <class T>
+T my_min(T a, T b)
+{
+	if(a < b)
+		return a;
+	else if(b < a)
+		return b ;
+	else
+		return a; // a and b are equivalent, so return a
+}
+
+int main( )
+{
+	int x{10};
+	long y{20};
+	std::cout << my_min(x, y) << std::endl;
+}
