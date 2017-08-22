8b42d6732c16634b4c805cbe8422ebc0ea7d5533
Bianca <bianca.rc@hotmail.com>
2017-08-22 19:12:41 -0300

    add questao 1

---
+++ problema_3/questao1.cpp
@@ -0,0 +1,29 @@
+#include <iostream>
+
+int main (int argc, char *argv[])
+{
+	int* pointer{new int {42}};
+	std::cout << *pointer << std::endl;
+
+	*pointer = 10;
+	std::cout << *pointer << std::endl;
+	delete pointer;
+	pointer = nullptr;
+
+	//std::cout << *pointer << std::endl;
+
+	long* good{new long{}};
+
+	// delete good;
+
+	good = nullptr;
+
+	// delete good;
+
+	long* bad{new long};
+	std::cout << *bad << std::endl;
+	delete bad;
+	bad = nullptr;
+
+	return EXIT_SUCCESS;
+}
