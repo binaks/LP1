f47d28f33f1bd9cfa0054ce145a42f735f9eea16
Bianca <bianca.rc@hotmail.com>
2017-08-22 21:49:51 -0300

    fix typo

--- problema_3/questao1.cpp
+++ problema_3/questao1.cpp
@@ -8,11 +8,13 @@ int main (int argc, char *argv[])
 	*pointer = 10;
 	std::cout << *pointer << std::endl;
 	delete pointer;
-	pointer = nullptr;
+//	pointer = nullptr;
 
-	//std::cout << *pointer << std::endl;
+	std::cout << *pointer << std::endl;
 
 	long* good{new long{}};
+	std::cout << *good << std::endl;
+	delete good;
 
 	// delete good;
 
