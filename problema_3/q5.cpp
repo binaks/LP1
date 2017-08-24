9e437f4b17a0cbdbbbcad88be1e06dd26670c3f2
winterScoming <kimura@live.it>
2017-08-24 19:53:49 -0300

    Add files via upload

---
+++ problema_3/q5.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+#include <new>
+
+struct Vector{
+	int tam;
+	double* elementos;
+};
+
+void vector_init(Vector& v, int tam){
+
+}
+
+void vector_free(Vector& v){
+
+}
+
+void vector_read(Vector& v){
+
+}
+
+double vector_sum(Vector& v){
+
+}
+
+bool vector_find(Vector& v, int key){
+
+}
+
+int main(int argc, char const *argv[])
+{
+	int n{0}, chave{0};
+	Vector vetor;
+	
+	return EXIT_SUCCESS;
+}
