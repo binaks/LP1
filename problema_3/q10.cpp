42588e874256ea6371cd9b9b94ec37e5a2642536
Renan <kimura@live.it>
2017-08-28 20:46:46 -0300

    Update q10.cpp
    eu não havia entendido o enunciado antes, mas agora está okay

--- problema_3/q10.cpp
+++ problema_3/q10.cpp
@@ -1,10 +1,9 @@
 #include <iostream>
 #include <fstream>
 #include <string>
-using namespace std;
 
-void arquivo_saida(ifstream& in, ofstream& out){ //não faz sentindo passar o valor por parametro
-	string linha, palavra, num;
+void arquivo_saida(std::ifstream& in, std::ofstream& out){
+	std::string linha, palavra, num;
 	size_t found, s, tam;
 	int n;
 	while(!in.eof()){
@@ -26,10 +25,10 @@ void arquivo_saida(ifstream& in, ofstream& out){ //não faz sentindo passar o va
 
 int main(int argc, char const *argv[])
 {
-	ifstream in("input.txt");
+	std::ifstream in(argv[1]);
 	if(not in)
-		perror("input.txt");
-	ofstream out("output.txt");
+		perror(argv[1]);
+	std::ofstream out(argv[2]);
 	arquivo_saida(in,out);
 	in.close();
 	out.close();
