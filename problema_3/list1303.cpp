ca4d98e84841d3e3916d0962d4597b6389911f68
Bianca <bianca.rc@hotmail.com>
2017-08-29 20:23:50 -0300

    add questao 9

--- problema_3/list1303.cpp
+++ problema_3/list1303.cpp
@@ -2,18 +2,23 @@
 #include <fstream>
 #include <iostream>
 
+using namespace std;
+
 int main(int argc, char const *argv[])
 {
-	std::ifstream in("list1301.in");
-	std::ofstream out("list1301.out");
+	std::ifstream in("list1303.in");
+	std::ofstream out("list1303.out");
 	if(not in)
-		std::perror("list1301.in");
+		std::perror("list1303.in");
 	else
 	{
 		int x(0);
 		while(in>>x)
 			out<<x<<'\n';
-		in.close();
+//		in.close();
 	}
+//	out.close();
+	if(out.is_open())
+		cout << "ta aberto" << endl;
 	return 0;
 }
