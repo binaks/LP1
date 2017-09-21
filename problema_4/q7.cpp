14cf7707cde37d1c15cd9b23921cb2e2dd54feab
Renan <kimura@live.it>
2017-09-21 20:09:30 -0300

    Add files via upload

--- problema_4/q7.cpp
+++ problema_4/q7.cpp
@@ -1,108 +1,82 @@
 #include <iostream>
 #include <new>
+using namespace std;
 
-
-class Vector
-{
-  public:
-
-  Vector() = delete;
-
-  Vector(int tam)
-	{
-    tam_ = tam;
-		elementos_ = new (std::nothrow) double[tam_];
-  
-    if(elementos_ == nullptr)
-			{
-				std::cout << "Falha na alocação" << std::endl;
-				exit(EXIT_FAILURE);
-			}
-	}
-  
-  ~Vector()
-  {
-    delete [] elementos_;
-		elementos_ = nullptr;
-    std::cout << "Vetor desalocado. " << std::endl;
-  }
-
-  void vector_read()
-  {
-    int i;
-    for(i = 0; i < tam_; i++)
-			{
-        std::cout << "Elementos[" << i+1 << "]: ";
-				std::cin >> elementos_[i];
-			}
-	}
-
-  double vector_sum()
-	{
-		double sum = 0;
-		int i;
-		for(i = 0; i < tam_; i++)
-			{
-				sum = sum + elementos_[i];
-			}
-   return sum;
-	}
-
-  bool vector_find(double key)
-	{
-    int i = 0;
-		for(i = 0; i < tam_; i++)
-			{
-				if(elementos_[i] == key)
-				{
-					return true;
-				}
-			}
-		return false;
-	}		
-
-  private:
+class Vector{
+private:
 	int tam_;
 	double* elementos_;
-
- 
 	
+public:
+	Vector() : tam_(0), elementos_(0) {};
+	// Vector() = delete;
+	Vector(int tam){
+		tam_ = tam;
+		elementos_ = new (std::nothrow) double[tam];
+		if(elementos_ == nullptr){
+			cout<<"Erro de inicialização.";
+		}
+	}
+	~Vector() {
+		delete [] elementos_;
+		elementos_ = nullptr;
+	}
+	int get_tam() { return tam_; }
+	double* get_elementos() { return elementos_; }
+	void set_tam(int tam) { tam_=tam; };
+	void set_elementos(double* elementos) { elementos_ = elementos; }
+
+	void vector_read(){
+		for (int i = 0; i < tam_; i++)
+		{
+			cout<<"Elemento ["<<i+1<<"]: ";
+			cin>>elementos_[i];
+		}
+	}
+	void vector_print(){
+		for (int i = 0; i < tam_; i++)
+		{
+			cout<<elementos_[i]<<" ";
+		}
+		cout<<endl;
+	}
+	double vector_sum(){
+		double sum;
+		for (int i = 0; i < tam_; i++)
+		{
+			sum += elementos_[i];
+		}
+		return sum;
+	}
+
+	bool vector_find(int key){
+		for (int i = 0; i < tam_; i++)
+		{
+			if(elementos_[i] == key){
+				return true;
+			}
+		}
+		return false;
+	}
 };
 
 int main()
 {
-  int n{0};
-	double chave{0};
-	
-	std::cout << "Entre com o número de elementos da sua lista: " << std::endl;
-	std::cin >> n;
+	int n{0}, chave{0};
 
-	Vector p1(n);
-  
-  p1.vector_read();
+	cout<<"Quantos elementos sua lista armazenara? ";
+	cin>>n;
+	Vector vetor = n;
+	vetor.vector_read();
 
-  std::cout << "Soma dos elementos informados: " << p1.vector_sum() << std::endl;
-
-  std::cout <<"Informe a chave de busca: "<< std::endl;
-	std::cin >> chave;
-
-  bool f;
-  f = p1.vector_find(chave);
-	
- if(f)
-		{
-			std::cout << "Chave Localizada!" << std::endl;
-		}
+	cout<<"\nVetor digitado: "; 
+	vetor.vector_print();
+	cout<<"Soma dos elementos informados: "<<vetor.vector_sum()<<endl;
+	cout<<"\nInforme chave de busca: ";
+	cin>>chave;
+	if(vetor.vector_find(chave)==true)
+		cout<<"Chave localizada!\n";
 	else
-		{
-			std::cout << "Chave não localizada!" << std::endl;
-		}
-
-
-  return EXIT_SUCCESS;
-  
+		cout<<"Chave nao localizada!\n";
+	return EXIT_SUCCESS;
 }
-
-
-
-
