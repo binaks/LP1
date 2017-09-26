17b4697da243612d3f7b700548b4c1f4870d6813
Renan <kimura@live.it>
2017-09-26 19:20:22 -0300

    Add files via upload

---
+++ problema_5/q1.cpp
@@ -0,0 +1,57 @@
+#include <iostream>
+
+template<class T>
+class point
+{
+public:
+	point(T const&, T const&);
+	point();
+	T const& x() const;
+	T const& y() const;
+	void move_to(T x, T y);
+	void move_by(T x, T y);
+private:
+	T x_;
+	T y_;
+	
+};
+
+template<class T>
+point<T>::point(T const& x, T const& y)
+: x_{x}, y_{y}
+{}
+
+template<class T>
+point<T>::point()
+: x_{x}, y_{y}
+{}
+
+template<class T>
+T const& point<T>::x()
+const
+{
+	return x_;
+}
+
+template<class T>
+T const& point<T>::y()
+const
+{
+	return y_;
+}
+
+template<class T>
+void point<T>::move_to(T x, T y)
+{
+	x_ = x;
+	y_ = y;
+}
+
+template<class T>
+void point<T>::move_by(T x, T y)
+{
+	x_ += x;
+	y_ += y;
+}
+
+int main(){}
